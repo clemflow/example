37ef26c9c021387250a21aa59878be0e9013b7b6
Ketan Bhave <Ketan.Bhave@docusign.com>
2019-02-06 14:15:51 -0800

    Added Generate Paranthesis method

--- c#/CodeBank.cs
+++ c#/CodeBank.cs
@@ -3,7 +3,6 @@
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-/// <$ tag=['test', 'add', 'method'] language=c#
 public class CodeBank
 {
 	public CodeBank()
@@ -12,5 +11,32 @@ public class CodeBank
 		// TODO: Add constructor logic here
 		//
 	}
+
+    /// <$ tag=['parenthesis', 'test', 'method'] language=c#
+    public static IList<string> GenerateParenthesis(int n)
+    {
+        List<string> result = new List<string>();
+        if (n <= 0) return result;
+        GenerateParenthesis(n, n, "", result);
+        return result;
+    }
+
+    private static void GenerateParenthesis(int left, int right, string item, List<string> result)
+    {
+        if (right < left) return;
+        if (left == 0 && right == 0)
+        {
+            result.Add(item);
+        }
+        if (left > 0)
+        {
+            GenerateParenthesis(left - 1, right, item + "(", result);
+        }
+        if (right > 0)
+        {
+            GenerateParenthesis(left, right - 1, item + ")", result);
+        }
+
+    }
+    // $>
 }
-// $>
